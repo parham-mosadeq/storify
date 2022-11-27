77f923bbf0059b35d7c4ca8c736b768b68c79bf3
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2022-11-27 22:31:37 +0330

    aboutUs don.

--- src/components/layouts/AboutUs.js
+++ src/components/layouts/AboutUs.js
@@ -1,10 +1,38 @@
 import React from 'react';
-
+//router
+import { Link } from 'react-router-dom';
+//icons
+import { FaGithub, FaLinkedin, FaMailBulk } from 'react-icons/fa';
+// styles
+import { MainAboutUsContainer } from '../../shared/AboutUsComponentStyled';
 const AboutUs = () => {
   return (
-    <div>
-      <h1>this is about us</h1>
-    </div>
+    <MainAboutUsContainer>
+      <div>
+        <h1>need to contact us?</h1>
+        <p>here is how you can reach out to us:</p>
+      </div>
+
+      <div>
+        <ul>
+          <li>
+            <Link href='#'>
+              <FaMailBulk />
+            </Link>
+          </li>
+          <li>
+            <Link href='#'>
+              <FaLinkedin />
+            </Link>
+          </li>
+          <li>
+            <Link href='#'>
+              <FaGithub />
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </MainAboutUsContainer>
   );
 };
 
---
+++ src/shared/AboutUsComponentStyled.jsx
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+
+const MainAboutUsContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2rem;
+  text-align: center;
+  position: relative;
+  height: 100vh;
+  z-index: 100;
+
+  div:first-child {
+    text-transform: capitalize;
+    letter-spacing: 1.2px;
+    font-weight: 200;
+    h1 {
+      font-weight: 400;
+    }
+
+    p {
+      color: #070789;
+    }
+  }
+
+  div:last-child {
+    ul {
+      display: flex;
+      width: 100%;
+      padding: 0;
+
+      li {
+        padding: 1rem;
+        list-style: none;
+
+        a {
+          font-size: 20px;
+          color: black;
+          transition: color 0.3s linear;
+          cursor: pointer;
+          :hover {
+            color: #070789;
+          }
+        }
+      }
+    }
+  }
+
+  ::before {
+    content: '';
+    width: 365px;
+    height: 365px;
+    position: absolute;
+    background-color: red;
+    border-radius: 50%;
+    opacity: 0.1;
+    top: -10px;
+    left: 20px;
+    z-index: 1;
+  }
+
+  ::after {
+    content: '';
+    width: 265px;
+    height: 265px;
+    position: absolute;
+    background-color: blue;
+    border-radius: 50%;
+    opacity: 0.2;
+    bottom: 10px;
+    right: 40px;
+    z-index: 2;
+  }
+
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    justify-items: center;
+    align-items: center;
+    margin-top: 10rem;
+  }
+`;
+
+export { MainAboutUsContainer };
